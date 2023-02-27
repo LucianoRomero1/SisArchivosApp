e29044258987b830e6303a70463c8628bd772b99
LucianoRomero1 <lucianooromero1@gmail.com>
2023-02-27 09:00:56 -0300

    Basic configs ready

--- src/components/private/PrivateLayout.jsx
+++ src/components/private/PrivateLayout.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 export const PrivateLayout = () => {
-  return (
-    <div>PrivateLayout</div>
-  )
-}
+  const { auth, loading } = useAuth();
+  if (loading) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <>
+        <section className="layout__content">
+          {auth._id ? <Outlet /> : <Navigate to="/login" />}
+        </section>
+      </>
+    );
+  }
+};
